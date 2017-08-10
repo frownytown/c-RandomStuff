30990cfd2876ed2b376bff1eccc1a9349700eba8
frownytown <rogerhmcclain@gmail.com>
2017-08-10 15:00:57 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,318 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Assignment3
+namespace Assignment5WordStats
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Main function creates dictionary for storage, before trying to run program
-            Dictionary<string, Member> dataStorage = new Dictionary<string, Member>();
+            
+            string filepath = @"..\resources\practice.txt";
+            string filepath_write = @"..\..\stats.txt";
+            
+            using (StreamReader streamreader = new StreamReader(filepath))
+            {
+                string fileText = streamreader.ReadToEnd();
+                // Count number of words in the file
 
-            try
-            {
-                dataStorage = StartUp(dataStorage);
-                foreach (KeyValuePair<string, Member> kvp in dataStorage)
-                {
-                    Console.WriteLine("@{0} @{1} @{2} @{3} @{4}", kvp.Key,
-                        kvp.Value.Sport, kvp.Value.ExpirationMonth, kvp.Value.ExpirationYear, kvp.Value.ExpiredStatus);
-                }
-                ProgramInterface(dataStorage);
-                Console.ReadKey();
-            }
-            catch
-            {
-                Console.Error.WriteLine("Something went wrong");
-            }
-        }
-        public static void SaveAndExit(Dictionary<string, Member> dataStorage)
-        {
-            // saves the database to a text file when you select to exit the program
-            try
-            {
-                // writer instance
-                string relativeFilePath = @"..\..\members.txt";
-                StreamWriter writer = new StreamWriter(relativeFilePath);
+                var count = fileText.Split(' ').Count();
 
-                using (writer)
+                // Get the average length
+                var averageLen = fileText.Split(' ').Average(n => n.Length);
+
+                // Dictionary storing <word, wordLength>
+                Dictionary<string, int> wordAndLength = new Dictionary<string, int>();
+
+                // Remove the special characters from the file, in order to get letter pairs
+                string longText = Extensions.RemoveSpecialCharacters(fileText);
+                var wordHold = fileText.Split(new char[] { ' ', '-', '.', '\\', '/', ':', ',', '(', ')', '"', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int x = 0; x < wordHold.Length; x++)
                 {
-                    foreach (KeyValuePair<string, Member> kvp in dataStorage)
+                    if (!wordAndLength.ContainsKey(wordHold[x]))
                     {
-                        writer.WriteLine("@{0} @{1} @{2} @{3} @{4}", kvp.Key, 
-                            kvp.Value.Sport, kvp.Value.ExpirationMonth, kvp.Value.ExpirationYear, kvp.Value.ExpiredStatus);
+                        wordAndLength.Add(wordHold[x], wordHold[x].Length);
                     }
                 }
-            }
-            catch
-            {
-                Console.WriteLine("Something went wrong, with the save and exit");
-                Console.ReadKey();
-            }
-        }
-        public static Dictionary<string, Member> StartUp(Dictionary<string, Member> dataStorage)
-        {
-            // Function that runs on startup to try and rebuild the saved database from the text file "members.txt"
-            try
-            {
-                string relativeFilePath = @"..\resources\members.txt";
-             
-                var textLines = File.ReadAllLines(relativeFilePath);
+                // sort wordAndLength dictionary by length
+                var sortedLengthDict = from value in wordAndLength orderby value.Value descending select value;
 
-                foreach (var line in textLines)
+                List<string> distinctPairs = new List<string>();
+                for (char c = 'a'; c <= 'z'; c++)
                 {
-                    string[] blah = line.Split('@');
-                    // rebuilds data from text file
+                    for (char j = 'a'; j <= 'z'; j++)
                     {
-                        string newName = blah[1];
-                        string newSport = blah[2];
-                        int newMonth = Convert.ToInt32(blah[3]);
-                        int newYear = Convert.ToInt32(blah[4]);
-                        bool newStatus = (blah[5].Contains("r"));
-                        
-                        Member newMember = new Member(newName, newSport, newMonth, newYear, newStatus);
-                        dataStorage.Add(newMember.Name, newMember);
-                       
+                        char[] tmpArr = { c, j };
+                        string search = new string(tmpArr);
+                        distinctPairs.Add(search);
                     }
                 }
-                
-                return dataStorage;
-            }
-            catch
-            {
-                Console.WriteLine("Something went wrong");
-                Console.WriteLine("Starting without loading text file...");
-                return dataStorage;
-            }
-        }
-        public static Dictionary<string, Member> AddMember(Dictionary<string, Member> dataStorage)
-        {
-            // Create dictionary for storage
-            Console.WriteLine("What is the Member's full name");
-            string newName = Console.ReadLine();
-            Console.WriteLine("What is the Member's preferred sport?");
-            string newSport = Console.ReadLine();
-            Console.WriteLine("What is the number of the month that this membership will expire (1 through 12)?");
-            int newMonth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the year number that this membership will expire?");
-            int newYear = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Is this person's membership expired?");
-            string response = Console.ReadLine();
-            bool newStatus = (response.Contains('y'));
+                string[] matchArray = distinctPairs.ToArray();
+                Dictionary<string, double> pairsAndOccurences = new Dictionary<string, double>();
 
-            Console.WriteLine("Press Y to confirm this addition or N to exit");
-            string confirm = Console.ReadLine();
-            if (confirm.Contains('y'))
-            {
-                Member newMember = new Member(newName, newSport, newMonth, newYear, newStatus);
-                dataStorage.Add(newMember.Name, newMember);
-                newMember.PrintInformation();
-            }
-            else
-            {
-                Console.Error.WriteLine("You have chosen to exit");
-                ProgramInterface(dataStorage);
-            }
-            return dataStorage;
-        }
-        public static Dictionary<string, Member> IsExpired(Dictionary<string, Member> dataStorage)
-        {
-            // Creates a dictionary of members that are expired
-            Dictionary<string, Member> expiredDict = new Dictionary<string, Member>();
-            foreach (KeyValuePair<string, Member> kvp in dataStorage)
-            {
-                if (kvp.Value.ExpiredStatus == true)
+                for (int i = 0; i < distinctPairs.Count; i++)
                 {
-                    Console.WriteLine("Name = {0}", kvp.Key);
-                    expiredDict.Add(kvp.Key, kvp.Value);
+                    int tmptest = Regex.Matches(longText, matchArray[i]).Cast<Match>().Count();
+                    pairsAndOccurences.Add(matchArray[i], tmptest);
                 }
-            }
-            Console.WriteLine("Are you an admin of this database?");
-            string adminStatus = Console.ReadLine();
-            if (adminStatus.Contains('y'))
-            {
-                Console.WriteLine("Excellent, press any key to continue");
-                Console.ReadKey();
-                Console.WriteLine("Please write the name of the person who's information you wish to edit");
-                string response = Console.ReadLine();
-                Console.WriteLine(response);
-                try
-                {
-                    foreach (KeyValuePair<string, Member> keyValue in dataStorage)
-                    {
 
-                        if (keyValue.Key.Contains(response))
-                        {
-                            Console.WriteLine("Press any key to confirm");
-                            Console.ReadKey();
-                            dataStorage.Remove(keyValue.Key);
-                            return dataStorage;
-                        }
-                    }
-                }
-                catch (KeyNotFoundException)
+                pairsAndOccurences = pairsAndOccurences.Where(p => p.Value != 0).ToDictionary(p => p.Key, p => p.Value);
+                List<int> foo = new List<int>();
+                foreach (var item in wordHold)
                 {
-                    Console.WriteLine("I couldn't find that person, Please try again");
-                    ProgramInterface(dataStorage);
+                    foo.Add(item.Length);
                 }
-            }
-            else
-            {
-                Console.WriteLine("ERROR! You must be an admin to complete this operation!");
-                ProgramInterface(dataStorage);
-            }
+                using (StreamWriter writer = new StreamWriter(filepath_write))
+                {
+                    Console.SetOut(writer);
+                    Console.WriteLine("The number of words in this file is: {0}", wordHold.Count());
+                    printLogicPairs.PairHistogram(pairsAndOccurences);
+                    printLogicLen.GoHistogram(foo);
 
-            return dataStorage;
-        }
-        public static Dictionary<string, Member> NotExpired(Dictionary<string, Member> dataStorage)
-        {
-            // Creates a dictionary of members that are not expired
-            Dictionary<string, Member> trimmedDict = new Dictionary<string, Member>();
-            foreach(KeyValuePair<string, Member> kvp in dataStorage)
-            {
-                if (kvp.Value.ExpiredStatus == false)
-                {
-                    trimmedDict.Add(kvp.Key, kvp.Value);
+                    Console.WriteLine("The average length is {0}", Math.Round(averageLen, 2));
+
                     
                 }
+                
             }
-            return trimmedDict;
         }
-        public static void PrefSports(Dictionary<string, Member> trimmedDict)
-        {
-            // THis function takes the dict of sports that are not expired and prints them.
-            Dictionary <string, int> histogram = new Dictionary<string, int>();
-            foreach (KeyValuePair<string, Member> sports in trimmedDict)
-            {
-                if (!histogram.ContainsKey(sports.Value.Sport))
-                {
-                    histogram[sports.Value.Sport] = 1;
-                }
-                else
-                {
-                    histogram[sports.Value.Sport]++;
-                }
-            }
-            foreach (KeyValuePair<string, int> item in histogram.OrderBy(key => key.Value))
-            {
-                int comparator = Convert.ToInt32(item.Value);
-                if ( comparator > 4)
-                {
-                    Console.WriteLine("These sports have more than 4 non expired players");
-                    Console.WriteLine("{0}", item);
-                    Console.ReadKey();
-                }
-            }
-           
-        }
-        public static Dictionary<string, Member> UpdateExpiration(Dictionary<string, Member> myDict)
-        {
-            foreach (KeyValuePair<string, Member> kvp in myDict)
-            {
-                Console.WriteLine("Name = {0}", kvp.Key);
-            }
-            Console.WriteLine("Are you an admin of this database?");
-            string adminStatus = Console.ReadLine();
-            if (adminStatus.Contains('y'))
-            {
-                Console.WriteLine("Excellent, press any key to continue");
-                Console.ReadKey();
-                Console.WriteLine("Please write the name of the person who's information you wish to edit");
-                string response = Console.ReadLine();
-                try
-                {
-                    foreach (KeyValuePair<string, Member> keyValue in myDict)
-                    {
 
-                        if (keyValue.Key.Contains(response))
-                        {
-                            Console.WriteLine("What is the number of the month that this membership will expire (1 through 12)?");
-                            myDict[keyValue.Key].ExpirationMonth = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("What is the year number that this membership will expire?");
-                            myDict[keyValue.Key].ExpirationYear = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("Is this person's membership expired?");
-                            string answer = Console.ReadLine();
-                            myDict[keyValue.Key].ExpiredStatus = (answer.Contains('y'));
-                            myDict[keyValue.Key].PrintInformation();
-                            return myDict;
-                        }
-                    }
-                }
-                catch (KeyNotFoundException)
-                {
-                    Console.WriteLine("I couldn't find that person, Please try again");
-                    ProgramInterface(myDict);
-                }
-            }
-            else
-            {
-                Console.WriteLine("ERROR, Only admin have access to this section!");
-                ProgramInterface(myDict);
-            }
-            
-            
-            return myDict;
-        }
-        public static Dictionary<string, Member> ProgramInterface(Dictionary<string, Member> dataStorage)
-        {
-            // user interface
-            Console.WriteLine("\n Welcome! How can I help you? Select a response and hit ENTER");
-            Console.WriteLine("1. Insert Member");
-            Console.WriteLine("2. Update a Member's Membership Expiration date");
-            Console.WriteLine("3. Find Preferred Sports");
-            Console.WriteLine("4. Delete Inactive Members");
-            Console.WriteLine("5. Save and Exit");
-            int choice = int.Parse(Console.ReadLine());
-            try
-            {
-                switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine("You have chosen to 1. Insert Member");
-                        AddMember(dataStorage);
-                        Console.WriteLine("Thank you for that, you will now return to the main screen");
-                        Console.ReadKey();
-                        ProgramInterface(dataStorage);
-                        break;
-                    case 2:
-                        Console.WriteLine("You have chosen to 2. Update a Member's Membership Expiration date");
-                        Console.ReadKey();
-                        UpdateExpiration(dataStorage);
-                        ProgramInterface(dataStorage);
-                        break;
-                    case 3:
-                        Console.WriteLine("You have chosen to 3. Find Preferred Sports.");
-                        Console.ReadKey();
-                        PrefSports(NotExpired(dataStorage));
-                        ProgramInterface(dataStorage);
-                        break;
-                    case 4:
-                        Console.WriteLine("You have chosen to 4. Delete Inactive members");
-                        Console.WriteLine("These people are inactive and may be deleted");
-                        IsExpired(dataStorage);
-                        ProgramInterface(dataStorage);
-                        break;
-                    case 5:
-                        Console.WriteLine("You have chosen to 5. Exit");
-                        Console.ReadKey();
-                        SaveAndExit(dataStorage);
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("default case");
-                        break;
-                }
-               
-            }
-            catch
-            {
-                Console.Error.WriteLine("Something went wrong");
-            }
-            return dataStorage;
-        }
+       
     }
 }
---
+++ printLogic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5WordStats
+{
+    public static class printLogicLen
+    {
+        public static SortedDictionary<int, int> GoHistogram(this IEnumerable<int> nums)
+        {
+            // contains the number of occurences of each size
+            var histoDict = new SortedDictionary<int, double>();
+            var dict = new SortedDictionary<int, int>();
+            foreach(var n in nums)
+            {
+                if (!dict.ContainsKey(n))
+                {
+                    dict[n] = 1;
+                }
+                else
+                {
+                    dict[n] += 1;
+                }
+            }
+            float valuesSum = dict.Values.Sum();
+            Console.WriteLine("\t Word Length Chart");
+            Console.WriteLine("----------------------------------");
+            foreach (var x in dict)
+            {
+                double histoFreq = (x.Value / valuesSum);
+                histoDict.Add(x.Key, histoFreq);
+                double preFrequency = (x.Value / valuesSum) * 100;
+                double frequency = Math.Round(preFrequency, 2);
+                Console.WriteLine("The length of [{0}] occurs {1} times, or % {2} of the time ", x.Key, x.Value, frequency);
+            }
+            
+            
+            
+            return dict;
+        }
+    }
+    public static class printLogicPairs
+    {
+        public static SortedDictionary<string, double> PairHistogram(this Dictionary<string, double> pairs)
+        {
+            // check the frequency of occurences for the pairs of letters
+            var pairDict = new SortedDictionary<string, double>();
+          
+            double sumPairs = pairs.Values.Sum();
+            Console.WriteLine("\t Word Occurence Chart");
+            Console.WriteLine("----------------------------------");
+            foreach (var j in pairs)
+            {
+                double prePair = (j.Value / sumPairs) * 100;
+                double pairFreq = Math.Round(prePair, 2);
+                pairDict.Add(j.Key, pairFreq);
+                Console.WriteLine("Letters [{0}] occur {1} times, or {2} % of the time ", j.Key, j.Value, pairFreq);
+            }
+            var mostPair = pairDict.OrderByDescending(kvp => kvp.Value).First().Key;
+            
+            Console.WriteLine("The highest percentage pair is: [{0}] percentage: % {1} of the time ", mostPair, pairDict[mostPair]);
+            return pairDict;
+            
+        }
+    }
+}
+
